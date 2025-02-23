2b1be68511d55803f2ab6a7705f5565a5a3e5a37
Vishal <vishalsinghrao140@gmail.com>
2025-02-24 00:43:02 +0530

    first commit

---
+++ src/String_Lowercase.java
@@ -0,0 +1,7 @@
+public class String_Lowercase {
+    public static void main(String[] args) {
+        String upper = "VISHAL";
+        System.out.println(upper.toLowerCase());
+        System.out.println(upper.replace(" " , "_"));
+    }
+}
