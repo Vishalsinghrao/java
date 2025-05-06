fabd888000f2df2226904b54e975f0207d0276c2
Vishal <vishalsinghrao140@gmail.com>
2025-05-06 23:45:32 +0530

    abstract methods

--- src/integer.java
+++ src/integer.java
@@ -1,10 +1,11 @@
 public class integer {
     public static void main(String[] args) {
-        int arr[] = {56,13,45,6,78,8};
-        for(int i=0;i< arr.length;i++){
-            if(arr[i] > arr[i+1]){
+        int arr[] = {56, 13, 45, 6, 78, 8};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
 
             }
         }
     }
+}
 
